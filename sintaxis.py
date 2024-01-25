5de595a0bd71a185915e8caf85b1c53a7a1e8b1a
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2024-01-24 20:23:42 -0600

    finalizamos la pract2

--- sintaxis.py
+++ sintaxis.py
@@ -5,8 +5,73 @@
 
 #soy un comentario de 1 linea
 
-'''soy un 
-comentarios 
-de 
-varias lineas
-'''
+# '''soy un 
+# comentarios 
+# de 
+# varias lineas
+# '''
+
+
+
+# 2. Cadenas
+
+# print('soy una cadena')
+# print("soy una cadena")
+
+# a= 'mi banda es \n favorita es'
+# b= "es grupo marrano "
+# print(a+b)
+# print(a,b) 
+
+#Contar los caracteres
+# print(len(a))
+
+
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+
+#3 Variables-- nunguna debe empezar con numeros 
+
+# x= int(3)
+# y= str("3")
+# z= float(3.2) 
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# #Numeros aleatorios
+# import random
+# numero= random.randrange(1,1500)
+# print(numero)
+
+
+#4. Solicitud de datos 
+
+# var1= input("introduce cualquier dato")
+# var2= str(input("introduce cadenas"))
+# var3= int(input("introduce solo enteros"))
+# var4= float(input("introduce numeros decimales"))
+
+# print(var1,var2,var3,var4)
+
+
+#5. Booleans, Operadores de comparacion y logicos 
+
+"""print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(10 >= 9)
+print(10 != 9)
+print(10 <= 9)"""
+
+#Y estos los logicos
+# x= 1
+# print(x < 5 and x< 10)
+# print(x < 5 or x< 10)
+# print(not(x < 5 and x< 10))
+
+#estos sirven para operaciones binarias 
+# print(x < 5 & x < 10)
+# print(x < 5 | x < 10)
+# print(not(x < 5 & x < 10))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+var1= input("introduce cualquier dato")
+var2= srt(input("introduce cadenas"))
+var3= int(input("introduce solo enteros"))
+var4= float(input("introduce numeros decimales"))
+
+print(var1,var2,var3,var4)
