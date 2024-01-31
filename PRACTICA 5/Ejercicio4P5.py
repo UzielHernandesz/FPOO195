005e85cb148c40edd62b5d3922be196f40131974
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2024-01-31 14:46:07 -0600

    Delete PRACTICA 5 directory

--- PRACTICA 5/Ejercicio4P5.py
+++
@@ -1,9 +0,0 @@
-# Escribir un programa que pida al usuario un número entero y muestre por
-# pantalla un triángulo rectángulo como el de más abajo, de altura el número
-# introducido. (img 1)
-
-numero_entero = int(input("Introduce la altura del triángulo (entero positivo): "))
-for i in range(1, numero_entero+1, 2):
-    for j in range(i, 0, -2):
-        print(j, end=" ")
-    print("")
