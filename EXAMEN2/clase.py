044f8fd82ba35d5db161b145c5f49dc802b02d63
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2024-03-11 20:23:49 -0600

    3

--- EXAMEN2/clase.py
+++ EXAMEN2/clase.py
@@ -1,47 +1,22 @@
 import sys
-from interfaz import interfaz 
-from Persona import Persona
-
 #Creamos la clase
 class alumno:
     
-    def __init__(self,esp,nom,apellidoP):
-        self.__especie= esp
-        self.__nombre= nom
-        self.__apellip= apellidoP
-        #_ despues del punto es para hacerlo privado 
-    #No va entre comillas por que queremos que sea flotante float
-    #_ ANTES DEL NOMBRE Y YA LOS HACEMOS PRIVADOS
-    #Getters (metodos Get)
-    
-    def get__especie(self):
-        return self.__especie
-    
-    def get__nombre(self):
-        return self.__nombre
-    
-    def get__apellip(self):
-        return self.__altura
-    
-    #Setters ( metodos Set)
-    
-    def set__especie(self, __especie):
-        self.__especie = __especie
-    
-    def set__nombre(self, __nombre):
-        self.__nombre = __nombre
-    
-    def set__apellip(self, __altura):
-        self.__altura= __altura
+    def __init__(self,apellidoM,nombreS,apellidoP,carrerA,añonamientO ):
+        self.nombre= nombreS
+        self.apellip= apellidoP
+        self.apellim= apellidoM
+        self.añonacimi= añonamientO
+        self.carre=carrerA
 
 #solicitar datos
-print("===============Datos del Alumno==============")
-nombreS= input("Escribe el nombre : ")
-apellidoP= input("Escribe el apellido Paterno: ")
-apellidoM= input (input("Escribe el apellido Materno: "))
-añonamientO= float(input("Escribe el año de nacimiento: "))
-carrerA= input("Escribe en que carrera estas: ")
-print("==============Datos del Alumno===============")
+#print("===============Datos del Alumno==============")
+#nombreS= input("Escribe el nombre : ")
+#apellidoP= input("Escribe el apellido Paterno: ")
+#apellidoM= input("Escribe el apellido Materno: ")
+#añonamientO= int(input("Escribe el año de nacimiento: "))
+#carrerA= input("Escribe en que carrera estas: ")
+#print("==============Datos del Alumno===============")
 
 
 
@@ -49,41 +24,11 @@ print("==============Datos del Alumno===============")
 alumno= alumno(nombreS,apellidoP,apellidoM,añonamientO,carrerA)
 
 
-#usamos los atributos del spartan
+#usamos los atributos del alumno
 print("============Informacion del alumno es: ==========")
-print(alumno.get__nombre())
-print(alumno.get__apellido_paterno())
-print(alumno.get__altura())
-print("")
-
-#usamos los atributos del nemesis
-print("============El objeto nemesis contiene==========")
-print(nemesis.get__nombre())
-print(nemesis.get__especie())
-print(nemesis.get__altura())
-
-
-#Usamos los metodos del spartan
-print("============ METODOS DEL OBJETO SPARTAN =============")
-spartan.correr(False)
-spartan.lanzar_Granada()
-print("")
-
-print("============ METODOS DEL OBJETO NEMESIS =============")
-nemesis.correr(True)
-nemesis.lanzar_Granada()
-print("")
-
-
-
-#Usamos los metodos del arma
-print("=====================SPARTAN===============")
-Arma.seleccionar_arma(spartan.get__nombre)
-Arma.Recargar_Arma(65)
-print("")
-
-
-print("=====================NEMESIS===============")
-Arma.seleccionar_arma(nemesis.get__nombre)
-Arma.Recargar_Arma(65)
+print(nombreS)
+print(apellidoP)
+print(apellidoM)
+print(añonamientO)
+print(carrerA)
 print("")
--- EXAMEN2/interfaz.py
+++ EXAMEN2/interfaz.py
@@ -1,10 +1,11 @@
 from tkinter import Tk,Frame, Button,Label,messagebox, Entry
 import tkinter as tk
+#from clase import clase
 
 
 class Login:
     
-
+    
         Ventana = Tk()
         Ventana.title("Login Persona")
         Ventana.geometry("800x600")
@@ -17,22 +18,30 @@ class Login:
         var1 = tk.StringVar()
         Label(seccion1, text="Nombre:", font=("Helvetica", 14)).pack()
         Entry(seccion1, takefocus=True, textvariable=var1).pack()
+        #nombreS= input("Escribe el nombre : ")
 
         var2 = tk.StringVar()
         Label(seccion1, text="Apellido Paterno:", font=("Helvetica", 14)).pack()
         Entry(seccion1, takefocus=True, textvariable=var1).pack()
-        
+        #apellidoP= input("Escribe el apellido Paterno: ")
+
         var3 = tk.StringVar()
         Label(seccion1, text="Apellido Materno", font=("Helvetica", 14)).pack()
         Entry(seccion1, takefocus=True, textvariable=var1).pack()
-        
+        #apellidoM= input("Escribe el apellido Materno: ")
+
+
         var4 = tk.StringVar()
         Label(seccion1, text="Año Nacimiento:", font=("Helvetica", 14)).pack()
         Entry(seccion1, takefocus=True, textvariable=var1).pack()
-        
+        #añonamientO= int(input("Escribe el año de nacimiento: "))
+
         var5 = tk.StringVar()
         Label(seccion1, text="Carrera:", font=("Helvetica", 14)).pack()
         Entry(seccion1, takefocus=True, textvariable=var1).pack()
+        #carrerA= input("Escribe en que carrera estas: ")
+        
+        
         
         
         messagebox.showinfo('Tu matricula es: ', ' No se pudo generar ')
